f9e4b5cb5d709320cac53d3b15a7e4ff98acdaef
Tristan <tristan.doering@tum.de>
2024-10-31 11:46:39 +0800

    create structure gpt lib

--- app/domain/song_vocabulary/lib/gpt_language_requests.rb
+++ app/domain/song_vocabulary/lib/gpt_language_requests.rb
@@ -5,6 +5,20 @@ module LyricLab
     # line credit calculation methods
     module GptLanguageRequests
       # hi carrie, you can start to implement the GPT library here
+      #
+
+      def self.extract_words(text)
+        # should return a list of mandarin words
+      end
+
+      def self.generate_example_sentence(word, language_level)
+        # should return a sentence with the word in it at the language level
+      end
+
+      def self.get_pinyin_translation(word)
+        # should return the pinyin and translation of the word
+        # in the format { pinyin: 'pinyin', translation: 'translation' }
+      end
     end
   end
 end
