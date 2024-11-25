1697978a6a9c2b3c19898956924251d18831eff8
Tristan <tristan.doering@tum.de>
2024-11-25 17:29:18 +0800

    structural changes to prepare for implementation

---
+++ app/infrastructure/lyric_lab_api.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative 'list_request'
+require 'http'
+
+module LyricLab
+  module Gateway
+    # Infrastructure to call CodePraise API
+    class Api
+    end
+  end
+end
