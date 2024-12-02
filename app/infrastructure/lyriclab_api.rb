fba124d249754bb83a7eef2ef000be8f671a4e71
Tristan <tristan.doering@tum.de>
2024-12-02 17:04:58 +0800

    small fixes

--- app/infrastructure/lyriclab_api.rb
+++ app/infrastructure/lyriclab_api.rb
@@ -16,8 +16,8 @@ module LyricLab
         @request.get_root.success?
       end
 
-      def list_recommendations()
-        @request.list_recommendations()
+      def list_recommendations
+        @request.list_recommendations
       end
 
       def load_search_results(search_query)
@@ -44,35 +44,35 @@ module LyricLab
       class Request
         def initialize(config)
           @api_host = config.API_HOST
-          @api_root = config.API_HOST + '/api/v1'
+          @api_root = "#{config.API_HOST}/api/v1"
         end
 
         def get_root # rubocop:disable Naming/AccessorMethodName
           call_api('get')
         end
 
-        def list_recommendations()
-            call_api('get', ['recommendations'])
+        def list_recommendations
+          call_api('get', ['recommendations'])
         end
 
         def load_search_results(query)
-            call_api('get', ['search_results'], 'search_query' => Value::Query.to_encoded(query))
+          call_api('get', ['search_results'], 'search_query' => Value::Query.to_encoded(query))
         end
 
         def load_song(origin_id)
-            call_api('get', ['songs', origin_id])
+          call_api('get', ['songs', origin_id])
         end
 
         def load_vocabulary(origin_id)
-            call_api('get', ['vocabularies', origin_id])
+          call_api('get', ['vocabularies', origin_id])
         end
 
         def record_recommendation(origin_id)
-            call_api('post', ['songs', origin_id])
+          call_api('post', ['songs', origin_id])
         end
 
         def load_songs(origin_ids)
-            call_api('get', ['search_results', origin_ids])
+          call_api('get', ['search_results', origin_ids])
         end
 
         private
@@ -97,7 +97,7 @@ module LyricLab
       class Response < SimpleDelegator
         NotFound = Class.new(StandardError)
 
-        SUCCESS_CODES = (200..299).freeze
+        SUCCESS_CODES = (200..299)
 
         def success?
           code.between?(SUCCESS_CODES.first, SUCCESS_CODES.last)
